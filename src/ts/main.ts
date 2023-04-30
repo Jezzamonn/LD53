a41cfaf87f7e76f9006f9d36d7f2562372c5597d
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2023-04-30 13:51:34 -0700

    Instructions to open the developer console

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,6 +3,18 @@ import { Game } from "./game/game";
 async function init() {
     await Game.preload()
 
+    window.addEventListener('click', hideTitle);
+}
+
+function hideTitle() {
+    const title = document.querySelector('.title') as HTMLElement;
+    title.classList.add('hidden');
+
+    const instructions = document.querySelector('.instructions') as HTMLElement;
+    instructions.classList.remove('hidden');
+
+    window.removeEventListener('click', hideTitle);
+
     const game = new Game('.canvas');
     game.start();
 }
